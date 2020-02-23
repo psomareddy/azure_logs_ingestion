a9b887401e610a8764c5fda41e1ea82c68e3111a
Prakash Reddy <preddy@newrelic.com>
2020-02-23 03:00:51 -0500

    fix typo

--- Constants.cs
+++ Constants.cs
@@ -13,7 +13,7 @@ namespace NewRelic.Azure
         public const string CONSUMER_GROUP = "newrelic_logs_consumer_group";
 
         //Do not edit this property
-        //The App Settings key that will specify the connection string( and not the actual connection string itself).
+        //The App Settings key that will specify the connection string (and not the actual connection string itself).
         public const string EVENT_HUB_CONNECTION_STRING = "EVENT_HUB_CONNECTION_STRING";
 
     }
